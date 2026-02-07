3b759cdad59ef82c564fd10f7f506164e1b3beb4
paulo.henrique90 <91470722+doggbeats@users.noreply.github.com>
2026-02-08 06:34:24 +0800

    Atualizar o AnimatedBackground.tsx

--- app/components/AnimatedBackground.tsx
+++ app/components/AnimatedBackground.tsx
@@ -18,57 +18,57 @@ export default function FundoAnimado() {
   if (!init) return null;
 
   return (
-
   <Particles
-  id="tsparticles"
-  className="fixed top-0 left-0 w-screen h-screen -z-10"
-  style={{ position: "fixed" }}
-  options={{
-    fullScreen: {
-      enable: true,
-      zIndex: -10,
-    },
-    fpsLimit: 60,
-    interactivity: {
-      events: {
-        onHover: {
-          enable: true,
-          mode: "grab",
-        },
+    id="tsparticles"
+    className="fixed top-0 left-0 w-screen h-screen -z-10"
+    style={{ position: "fixed" }}
+    options={{
+      fullScreen: {
+        enable: true,
+        zIndex: -10,
       },
-      modes: {
-        grab: {
-          distance: 200,
-          links: {
-            opacity: 0.5,
-            color: "#17e810",
+      fpsLimit: 60,
+      interactivity: {
+        events: {
+          onHover: {
+            enable: true,
+            mode: "grab",
+          },
+        },
+        modes: {
+          grab: {
+            distance: 200,
+            links: {
+              opacity: 0.5,
+              color: "#17e810",
+            },
           },
         },
       },
-    },
-    particles: {
-      color: { value: "#a5c9ff" },
-      links: {
-        color: "#a5c9ff",
-        distance: 150,
-        enable: true,
-        opacity: 0.2,
-        width: 1,
+      particles: {
+        color: { value: "#a5c9ff" },
+        links: {
+          color: "#a5c9ff",
+          distance: 150,
+          enable: true,
+          opacity: 0.2,
+          width: 1,
+        },
+        move: {
+          enable: true,
+          speed: 1,
+          direction: "none",
+          outModes: { default: "out" },
+        },
+        number: {
+          density: { enable: true },
+          value: 100,
+        },
+        opacity: { value: 0.3 },
+        shape: { type: "circle" },
+        size: { value: { min: 1, max: 3 } },
       },
-      move: {
-        enable: true,
-        speed: 1,
-        direction: "none",
-        outModes: { default: "out" },
-      },
-      number: {
-        density: { enable: true },
-        value: 100,
-      },
-      opacity: { value: 0.3 },
-      shape: { type: "circle" },
-      size: { value: { min: 1, max: 3 } },
-    },
-    detectRetina: true,
-  }}
-/>
+      detectRetina: true,
+    }}
+  />
+);
