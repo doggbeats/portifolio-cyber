e3893bc8ac1a84de08a77862f9d2ab6348cc26b3
Paulo Henrique <paulo.analise90@gmail.com>
2026-08-07 17:09:32 -0300

    Corrige next config e remove dados sensiveis

--- next.config.ts
+++ next.config.ts
@@ -5,17 +5,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-
-git remote add origin https://github.com/doggbeats/portifolio-cyber
-git branch -M main
-git push -u origin main
-
-
-Projeto desenvolvido em Next.js (React) com JavaScript, utilizando IA generativa como apoio no desenvolvimento, otimização de código e aplicação de boas práticas.
-https://api.whatsapp.com/send?phone=61992890326
-
-
-senha banco de dados Cyber$$dogg$
-
-doggbeats's Project
